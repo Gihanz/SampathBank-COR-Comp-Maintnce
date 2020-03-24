33f3858c413704ae297bb7d4b17b73caba9f3b15
sunethakalanka <akalanka.suneth6@gmail.com>
2020-03-25 00:16:11 +0530

    create DTO class

--- src/main/java/biz/nable/sb/cor/comp/response/UserFeaturesResponse.java
+++ src/main/java/biz/nable/sb/cor/comp/response/UserFeaturesResponse.java
@@ -1,27 +1,16 @@
 package biz.nable.sb.cor.comp.response;
 
-
 import biz.nable.sb.cor.common.response.CommonResponse;
-import biz.nable.sb.cor.common.utility.StatusEnum;
-import biz.nable.sb.cor.comp.utility.RecordStatusEnum;
-
 import lombok.Data;
 import lombok.ToString;
 
 import java.util.Date;
-import java.util.List;
 
 @Data
 @ToString
-public class UserCommonResponse {
+public class UserFeaturesResponse  {
 
     private Long id;
-    private String userName;
-    private String designation;
-    private Long branch;
-    private String remark;
-    private StatusEnum status;
-    private RecordStatusEnum recordStatus;
     private  String createdBy;
     private Date createdDate;
     private String lastUpdatedBy;
@@ -29,6 +18,4 @@ public class UserCommonResponse {
     private  Date lastVerifiedDate;
     private  String lastVerifiedBy;
     private  String userGroup;
-    private List<CompanyUserResponse> companyUsers;
-
 }
